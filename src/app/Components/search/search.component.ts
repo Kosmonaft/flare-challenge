c669b06879361959525de90824ef83b01dd854f9
Pawel Christow <pawel.christow@gmail.com>
2019-09-15 15:04:27 +1000

    Create searchForm in searchComponent and add autocomplete input from AngularMaterial

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  searchForm: FormGroup;
+  constructor() {}
 
   ngOnInit() {
+    this.initSearchForm();
   }
 
+  initSearchForm() {
+    this.searchForm = new FormGroup({
+      searchInput: new FormControl(null)
+    });
+  }
 }
