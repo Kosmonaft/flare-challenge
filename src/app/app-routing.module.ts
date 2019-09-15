bf295ae555969ed075cea7c2957bb3303d09b986
Pawel Christow <pawel.christow@gmail.com>
2019-09-15 14:59:35 +1000

    Add route and history routes in the app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from '../app/Components/search/search.component';
+import { HistoryComponent } from '../app/Components/history/history.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SearchComponent
+  },
+  {
+    path: 'history',
+    component: HistoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
