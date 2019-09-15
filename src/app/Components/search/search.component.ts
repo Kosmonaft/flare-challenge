f48549cd459baba0c94d8470c19aba1ba4976091
Pawel Christow <pawel.christow@gmail.com>
2019-09-15 15:48:09 +1000

    Add simple autocomplete with static data

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { IRestCountries } from '../../Models/rest-countries';
+import { ISearchAutocompleteOption } from '../../Models/search';
+import { debounceTime, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,6 +12,8 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
   searchForm: FormGroup;
+  options: ISearchAutocompleteOption[];
+
   constructor() {}
 
   ngOnInit() {
@@ -17,5 +24,28 @@ export class SearchComponent implements OnInit {
     this.searchForm = new FormGroup({
       searchInput: new FormControl(null)
     });
+    this.subscribeToSearchInputChange();
+  }
+
+  subscribeToSearchInputChange(): void {
+    this.searchForm
+      .get('searchInput')
+      .valueChanges.pipe(
+        debounceTime(250), // Adding debounce to prevent API calls on each value change (reduce API calls)
+        switchMap(value => {
+          return value.length >= 3 ? of(this.getCountries()) : of(undefined);
+        })
+      )
+      .subscribe((autocompleteSuggestions: any) => {
+        if (autocompleteSuggestions) {
+          this.options = autocompleteSuggestions;
+        } else {
+          this.options = [];
+        }
+      });
+  }
+
+  getCountries(): any {
+    return ['Australia', 'new zeealand'];
   }
 }
